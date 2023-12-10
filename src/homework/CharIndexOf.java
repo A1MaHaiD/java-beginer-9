b8bf9c2bc0d85321812769c7d4ef754240e9ebc5
A1MaHaiD <super.lukianchuk2010@gmail.com>
2023-12-11 01:06:27 +0200

    create CHARINDEXOF.md && CharIndexOf.java

---
+++ src/homework/CharIndexOf.java
@@ -0,0 +1,12 @@
+package homework;
+
+import java.util.Scanner;
+
+public class CharIndexOf {
+    public static void main(String[] args) {
+        // PUT YOUR CODE HERE
+        String sourceStr = new Scanner(System.in).nextLine();
+        System.out.println(sourceStr.charAt(3));
+        System.out.println(sourceStr.indexOf('a'));
+    }
+}
