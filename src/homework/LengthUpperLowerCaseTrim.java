bc246c83ef9aadc88aacf32d553e2762019c0723
A1MaHaiD <super.lukianchuk2010@gmail.com>
2023-12-11 00:54:34 +0200

    inflate LengthUpperLowerCaseTrim.java && LENUPLOWTRIM.md

--- src/homework/LengthUpperLowerCaseTrim.java
+++ src/homework/LengthUpperLowerCaseTrim.java
@@ -1,5 +1,14 @@
 package homework;
 
-public class LengthUpperLowerCaseTrim {
+import java.util.Scanner;
 
+public class LengthUpperLowerCaseTrim {
+    public static void main(String[] args) {
+        // PUT YOUR CODE HERE
+        String inputStr = new Scanner(System.in).nextLine();
+        System.out.println(inputStr.length());
+        System.out.println(inputStr.toUpperCase());
+        System.out.println(inputStr.toLowerCase());
+        System.out.println(inputStr.trim());
+    }
 }
